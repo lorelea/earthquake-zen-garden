0282c760ff676a865a30dba21c8b424f19eff642
lorelea <shaynalekohmaher@gmail.com>
2021-06-08 06:47:27 -0700

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# earthquake-zen-garden
-
-## Get started
-- `npm install` - installing dependencies & husky Git-hooks
-- `npm run start` - start the app on `http://localhost:3000/` and open in browser
--- src/store/slices/selectors/site.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-
-const selectConfig = (state) => state.siteConfig;
-
-export const getSiteConfig = createSelector([selectConfig], (siteConfig) => siteConfig);
-
-export default { getSiteConfig };
